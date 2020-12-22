01115f7f6f6570f6fcaefa1b170b2b6a08297007
jakob <jakob.duehr@gmx.net>
2020-12-22 18:13:18 +0100

    - ServerController ist nun ein Thread und blockiert die GUI nicht mehr (neue Funktionen: run())
    ChatController: neue Funktionen: sendMessage, sendNickname (um diese beim Drücken von ENTER aufzurufen)
    
    LoginController: addEventFilter zu Scene hinzugefügt, um KeyEvents abzufangen (line 69-86) -> man kann nun beide Textfelder mit Enter bestätigen und muss nicht den Knopf drücken

--- src/ChatApplication/ChatApp.java
+++ src/ChatApplication/ChatApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class ChatApp extends Application {
@@ -12,18 +13,28 @@ public class ChatApp extends Application {
     public void start(Stage primaryStage) throws Exception{
         // get root
         Parent loginFormRoot = FXMLLoader.load(getClass().getResource("Resource/loginForm.fxml"));
+        System.out.println("test");
 
         // show and start controller
         primaryStage.setTitle("ChatApp");
+
         primaryStage.setScene(new Scene(loginFormRoot));
         primaryStage.show();
     }
 
     @Override
     public void stop() throws Exception {
-        super.stop();
-        System.out.println("stop");     // todo
-        System.exit(0);
+        try {
+            super.stop();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("stop"); // todo
+        }
+
+
+
+        //System.exit(0);
     }
 
     public static void main(String[] args) {
