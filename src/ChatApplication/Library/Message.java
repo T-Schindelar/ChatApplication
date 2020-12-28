c87790f3f3bd13386213bff9fdcd38ce01b63fef
Tobias <schindus89@gmx.de>
2020-12-28 09:30:02 +0100

    Mode zur Message KLasse hinzugefügt + Mode erstellt als enum

--- src/ChatApplication/Library/Message.java
+++ src/ChatApplication/Library/Message.java
@@ -6,17 +6,22 @@ import java.util.Date;
 public class Message implements java.io.Serializable {
     private final String timestamp;
     private final String client;
+    private final Mode mode;
     private final String text;
 
-    public Message(String client, String text) {
+    public Message(String client, Mode mode, String text) {
         this.timestamp = new SimpleDateFormat("HH:mm").format(new Date());
         this.client = client;
+        this.mode = mode;
         this.text = text;
     }
 
     // getter
     public String getClient() {
         return client;
+    }
+    public Mode getMode() {
+        return mode;
     }
     public String getText() {
         return text;
---
+++ src/ChatApplication/Library/Mode.java
@@ -0,0 +1,13 @@
+package ChatApplication.Library;
+
+// mode for a message
+public enum Mode {
+    ERROR,
+    CHANGE_NAME,
+    CHANGE_PASSWORD,
+    LOGIN,
+    LOGOUT,
+    MESSAGE,
+    REGISTRATION
+}
+
